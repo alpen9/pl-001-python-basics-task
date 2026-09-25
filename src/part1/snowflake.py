9eb51cfdb614290d55135d34299fba51d39f3b95
alpen9 <alyapenskaya@yandex.ru>
2026-09-25 21:40:01 +0300

    Solve task 1

--- src/part1/snowflake.py
+++ src/part1/snowflake.py
@@ -120,7 +120,7 @@ def generate_snowflake_id(
         print(f"node_id must be in [0, {NODE_ID_MAX}]")
         return None
     if not (0 <= sequence_id <= SEQUENCE_ID_MAX):
-        print(f"sequence_id must be in [0, {SEQUENCE_ID_MAX}")
+        print(f"sequence_id must be in [0, {SEQUENCE_ID_MAX}]")
         return None
     timestamp = read_current_millis(epoch_ms)
     if not (0 <= timestamp <= TIMESTAMP_MS_MAX):
